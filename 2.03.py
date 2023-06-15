c7d798ea2001cb67af8eefa5bec5f186dff5c5b5
Archangel <86256017+AxxlYT@users.noreply.github.com>
2023-06-15 09:19:17 -0400

    Delete 2.03.py

--- 2.03.py
+++
@@ -1,8 +0,0 @@
-#Name: Azel (Axxl)
-#Date: 09:52 EST 31/3/23
-#File name: 2.03.py
-#Description: Prints a list of cast members from a selected movie in Python
-
-print("="*30 + "CAST OF ENDGAME" + "="*30 + "\n" "Iron Man/Tony Stark\t" + "Robert Downey JR\n" + "Black Window\t" + 
-      "Scarlett Johansson\n" "Captain America\t" + "Chris Evans\n" + "Thor\t" + "Chris Hemsworth\n" + "Hulk/Bruce Banner\t" + "Mark Ruffalo\n"
-      "Hawkeye\t" + "Jeremy Renner\n" + "Captain Marvel\t" + "Brie Larson\n" + "Antman\t" + "Paul Rudd\n" + "="*75)
