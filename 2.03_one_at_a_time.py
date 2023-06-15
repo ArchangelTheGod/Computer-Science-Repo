28d02494d678fc50979e85bc36b7dd2dd951d4e9
Archangel <86256017+AxxlYT@users.noreply.github.com>
2023-06-15 09:19:26 -0400

    Delete 2.03_one_at_a_time.py

--- 2.03_one_at_a_time.py
+++
@@ -1,8 +0,0 @@
-#Name: Azel (Axxl)
-#Date: 3/4/32
-#File Name: 2.0.3_one_at_a_time.py
-#Description: Part 2??? Maybe?
-
-print("="*30  + "CAST OF ENDGAME" + "="*30 + "Iron Man/Tony Stark\t" + "Aiden\n" + "Black Window\t" + 
-      "Sonya lauzon\n" "Captain America\t" + "\n" + "Thor\t" + "Chris Hemsworth\n" + "Hulk/Bruce Banner\t" + "\n"
-      "Hawkeye\t" + "Jeremy Renner\n" + "Captain Marvel\t" + "Brie Larson\n" + "Antman\t" + "Paul Rudd\n")  
