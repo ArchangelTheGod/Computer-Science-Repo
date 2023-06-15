a993eab125c34f761ca23440f9232aabb4b44f71
Archangel <86256017+AxxlYT@users.noreply.github.com>
2023-06-15 09:20:39 -0400

    Delete 2.06b_05_basicMath.py

--- 2.06b_05_basicMath.py
+++
@@ -1,5 +0,0 @@
-#Name: Azel (Axxl)
-#Date: 17/4/23
-#File Name: 2.06b_05_basicMath.py
-#Description: Asks the user to provide the radius of a circle which will then be calculated into circumference and area.
-
