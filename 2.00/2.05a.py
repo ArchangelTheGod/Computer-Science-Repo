b7b90ef8a99cf46790136933cfb7a6992c8c398d
Archangel <86256017+AxxlYT@users.noreply.github.com>
2023-06-15 09:22:02 -0400

    Add files via upload

---
+++ 2.00/2.05a.py
@@ -0,0 +1,13 @@
+#Name: Azel (Axxl)
+#Date: 04/04/23
+#File Name:2.05a.py
+#Description:Not sure yet?
+
+#Variables
+a = 5
+b ="NAME"
+c = 4.3232
+d = "4.3232"
+e ="true"
+f = 1+2+3+4+5+6+7+8+9
+g = 4.3232 + 4.1234 + 2.3212
---
+++ 2.00/2.06b_03_basicMath.py
@@ -0,0 +1,25 @@
+#Name: Azel (Axxl)
+#Date: 17/4/23
+#File Name: 2.06b_03_basicMath.py
+#Description: Prompts the user to enter 2 intergers and prints an equation with the provided intergers.
+
+#Inputs
+num1 = int(input("Specify a number ->"))
+num2 = int(input("Specify a second variable ->"))
+
+#Calculations
+ans1 = num1+num2
+ans2 = num1-num2
+ans3 = num1*num2
+ans4 = num1/num2
+
+#Output
+print(f"Equation 1: {ans1}")
+print("\n")
+print(f"Equation 2: {ans2}")
+print("\n")
+print(f"Equation 3: {ans3}")
+print("\n")
+print(f"Equation 4: {ans4}")
+print("\n")
+print("-- Hey! --")
